a9f754c95f5c00930c3b7860d4adfc4a4d71b3ee
Scottie Blangeres <scottblangeres13@gmail.com>
2022-09-26 22:21:07 -0700

    Initial commit with all contents

---
+++ README.md
@@ -0,0 +1,12 @@
+# 05 Third-Party APIs: Work Day Scheduler
+
+This site is designed to let you plan events for your day, basically this site is a planner. Once you load
+the page, at the top you will see the current date, then you will see timeblocks for 9am to 5pm (normal work hours)
+that are color coded depending on which hours have already passed, the current hour, and finally the future hours.
+You can enter text into these boxes and save it to local storage by pressing the save button. The data will persist
+even if you leave the site. 
+
+## Deployed Page
+[URL Of Deployed Application]()
+
+![Screenshot](siteSS.png)
---
+++ assets/script.js
@@ -0,0 +1,92 @@
+var m = moment();
+var formattedDate = m.format('dddd, MMMM Do YYYY');
+var currentHour = m.format('H');
+var dateText = $('#currentDay');
+var saveData = [];
+
+dateText.text(formattedDate);
+
+//Creating the time blocks
+function createHours(){
+    for (var hour = 9; hour < 18; hour++){
+        var hourEl = document.createElement("div");
+        var row = document.createElement("div");
+        var hourBoxEl = document.createElement("div");
+        var descEl = document.createElement("div");
+        var timingEl = document.createElement("div");
+        var textBox = document.createElement("textarea");
+        var saveBtn = document.createElement("button");
+
+        hourEl.setAttribute('id', String(hour));
+        saveBtn.textContent = "SAVE";
+        hourEl.className = "time-block";
+        row.className = "row";
+        hourBoxEl.className = "hour";
+        descEl.className = "description";
+        saveBtn.className = "saveBtn";
+
+        //Formatting displayed time for time blocks
+        if (hour > 12){
+            hourBoxEl.textContent = hour - 12 + "PM";
+        } else {
+            hourBoxEl.textContent = hour + "AM";
+        }
+
+        //Changing color of time blocks based on current time
+        if (hour == currentHour){
+            timingEl.className = "present";
+        }
+        else if (hour < currentHour){
+            timingEl.className = "past";
+        }
+        else {
+            timingEl.className = "future";
+        }
+
+         $('.container').append(hourEl);
+         hourEl.append(row);
+         row.append(hourBoxEl);
+         row.append(descEl);
+         row.append(saveBtn);
+         descEl.append(timingEl);
+         timingEl.append(textBox);
+
+    }
+}
+
+//Loading all saved data
+function load(){
+    var inputs = JSON.parse(localStorage.getItem('inputs'));
+    saveData = inputs;
+    console.log(saveData)
+    inputs.forEach(function(input){
+        var el = $('#' + input.id);
+        el.find('textarea').val(input.text);
+    });
+    //Getting rid of empty datasaves on page refresh
+    for(var i = 0; i < saveData.length; i++){
+        if (saveData[i].text.trim().length < 1){
+            saveData.splice(i, 1);
+        }
+    }
+}
+
+
+createHours();
+load();
+
+//Saving the contents of chosen hour on click of save button
+$('.saveBtn').on("click", function(event){
+    var rowEl = $(this).parent();
+    var textInput = rowEl.find("textarea").val();
+    var thisId = $(this).parents('.time-block').attr('id');
+    //removing old data saves for same hour
+    for(var i = 0; i < saveData.length; i++){
+        if (thisId == saveData[i].id){
+            saveData.splice(i,1);
+        }
+    }
+    saveData.push({id: thisId, text: textInput})
+    localStorage.setItem('inputs', JSON.stringify(saveData));
+})
+
